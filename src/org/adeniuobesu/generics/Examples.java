565970652f1fdeebd8f49aa4a59a58d09e71438f
MOUKHAFI Anass <moukhafi.anass@gmail.com>
2022-10-24 14:07:31 +0100

    Creation du projet

---
+++ src/org/adeniuobesu/generics/Examples.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.adeniuobesu.generics;
+
+/**
+ * @author MOUKHAFI Anass
+ * 
+ * Oct 22, 2022
+ */
+public class Examples {
+	public static void main(String[] args) {
+		System.out.println("Hey everybody!");
+	}
+}
