b36f07014c791a2f0966e21c15cc0bb039aab23e
MOUKHAFI Anass <moukhafi.anass@gmail.com>
2022-11-08 18:11:37 +0100

    Tested all LinkedList's methods in a main class

--- src/org/adeniuobesu/generics/Main.java
+++ src/org/adeniuobesu/generics/Main.java
@@ -14,8 +14,10 @@ package org.adeniuobesu.generics;
  */
 public class Main {
 	
-	List<Integer> integerLinkedList, integerVector;
-	Set<Integer> integerLinkedSet;
+	List<Integer> list;
+	Set<Integer> set;
+	// We'll need a temporary list to compare it with our main list [LinkedList, Vector]
+	List<Integer> tempList;
 	
 	public Main() {
 		exp01();
@@ -24,16 +26,48 @@ public class Main {
 	}
 	// Tests all LinkedList's features
 	private void exp01() {
-		integerLinkedList = new LinkedList<>();
-		integerLinkedList.add(2);
-		integerLinkedList.add(15);
-		integerLinkedList.add(3);
-		integerLinkedList.add(2, 4);
-		integerLinkedList.add(2, 8);
-		System.out.println(integerLinkedList);
-		integerLinkedList.clear();
-		System.out.println(integerLinkedList);
-		integerLinkedList = null;
+		list = new LinkedList<>();
+		
+		System.out.println(list.isEmpty()); // prints true
+		list.add(3);
+		System.out.println(list.isEmpty()); // prints false
+		list.add(0, 4); // 4-3
+		
+		List<Integer> tempList = new LinkedList<>();
+		tempList.add(4);
+		tempList.add(7);
+		
+		System.out.println(list.contains(4)); //prints true
+		System.out.println(list.contains(null)); // prints false
+		System.out.println(list.contains(8)); // prints false
+		
+		System.out.println(list.size()); //prints 2
+		
+		System.out.println(list); // prints (4-3).
+		
+		list.add(1, 7); // list (4-7-3) & tempList (4-7)
+		// Compare elements's existence
+		System.out.println(list.containsAll(tempList)); // prints true
+		System.out.println(list.equals(tempList)); // prints false
+		tempList.add(3);
+		System.out.println(list.equals(tempList)); // prints true
+		
+		System.out.println(list.set(0, 12)); // removes 4 and sets 12
+		System.out.println(list.get(0)); // prints 12
+		list.add(19);// Our list is now containing (12-7-3-19)
+		
+		System.out.println(list.subList(1, 3)); // subList contains (7-3-19)
+		System.out.println(list.remove(1)); // removes and prints 7
+		
+		System.out.println(list.indexOf(12)); // prints 0
+		System.out.println(list); // 12-3-19
+		System.out.println(list.indexOf(19, 1)); // starts searching from position 1 returns 2
+		
+		list.clear(); // empties (clears) the list
+		System.out.println(list); // prints LinkedList contains no elements.
+		list = null;
+		tempList.clear();
+		tempList = null;
 	}
 	
 	// Tests all Vector's features
